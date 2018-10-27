94eeb329c1ba8e9a3895528a3a3815a40541cb20
kim <rlaqudrhks258@naver.com>
2018-10-27 12:43:05 +0900

    20181026- 스프링1일차 과정

--- src/main/java/polymorphism/SamsungTV.java
+++ src/main/java/polymorphism/SamsungTV.java
@@ -1,16 +1,34 @@
 package polymorphism;
 
 public class SamsungTV implements TV {
-	public void initMethod() {
+/*	public void initMethod() {
 		System.out.println("객체 초기화 적업 처리");
 	}
+	//
+	public void destroyMethod() {
+		System.out.println("객체 삭제 전에 처리할 로직 처리");
+	}*/
+	private SonySpeaker speaker;
+	private int price;
+	
 	public SamsungTV() {
-		System.out.println("==> samsungtv 객체 생성");
+		System.out.println("==> samsungtv(1) 객체 생성");
 	}
+	//생성자의 매개변수에 의존관계에 있는 개체의 주소 정보를 전달!
+	public SamsungTV(SonySpeaker speaker) {
+		System.out.println("===>SamsungTV(2) 객체 생성");
+		this.speaker=speaker; 
+	}
+	public SamsungTV(SonySpeaker speaker, int price) {
+		System.out.println("===>SamsungTV(3) 객체 생성");
+		this.speaker=speaker;
+		this.price=price;
+	}
+	
 
 	@Override
 	public void powerOn() {
-		System.out.println("삼성티비 전원 킨다");
+		System.out.println("삼성티비 전원 킨다(가격 : "+price+")");
 		
 	}
 
@@ -20,17 +38,16 @@ public class SamsungTV implements TV {
 		
 	}
 
-	@Override
 	public void volumeUp() {
-		System.out.println("삼성티비 볼륨높인다.");
 		
+		speaker.volumeUp();
 	}
-
-	@Override
 	public void volumeDown() {
-		System.out.println("삼성티비 볼륨낮춘다.");
 		
+		speaker.volumeDown();
 	}
+	
+
 	
 
 }
---
+++ src/main/java/polymorphism/SonySpeaker.java
@@ -0,0 +1,14 @@
+package polymorphism;
+
+public class SonySpeaker {
+	//积己磊.
+	public SonySpeaker() {
+		System.out.println("===> SonySpeaker按眉 积己");
+	}
+	public void volumeUp() {
+		System.out.println("SonySpeaker ---杭俘 棵府扁");
+	}
+	public void volumeDown() {
+		System.out.println("SonySpeaker ---杭俘 郴府扁");
+	}
+}
